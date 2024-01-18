49ed2c2ae6358c65e952dbc16775dd186acbd878
priyans619 <priyansh.2b@gmail.com>
2024-01-18 22:05:11 +0530

    update mongoose conneection to PORT || mongoURL

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { PORT, mongoURL } from './config.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
 app.get('/');
 
-
+// for loading process.env from config
+dotenv.config();
 
 mongoose
-.connect(mongoURL)
+.connect(process.env.mongoURL)
 .then(() => {
     console.log('Server is connected to database');
-    app.listen(PORT, () => {
-      console.log('App is listening to port: ${PORT}')
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening to port: ${process.env.PORT}`)
     })
 })
 .catch((error) => {
