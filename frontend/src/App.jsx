40963a08c0f24919848d5926f6b24fcd9b61eefa
priyans619 <priyansh.2b@gmail.com>
2024-01-19 23:55:24 +0530

    Update font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,16 @@ import SupportTicket from './components/SupportTicket';
 
 function App() {
   return (
+
     <div className="App">
-      <h1 className="max-w-md mx-auto mt-8 ">Support Agent Creation Screen</h1>
+      <div className="flex items-center justify-center">
+        <h1 className="mr-10 text-3xl mt-4 font-bold">Support Ticket Entry System</h1>
+      </div>
+
+      <h1 className="max-w-md mx-auto mt-8 font-bold">Support Agent Creation Screen</h1>
       <SupportAgent />
 
-      <h1 className="max-w-md mx-auto mt-8">Support Ticket Entry Screen</h1>
+      <h1 className="max-w-md mx-auto mt-8 font-bold">Support Ticket Entry Screen</h1>
       <SupportTicket />
     </div>
 
