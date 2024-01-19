c482ce40ca853243487ee6bc681fc64408ad31dd
priyans619 <priyansh.2b@gmail.com>
2024-01-19 23:37:43 +0530

    Update App.jsx render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import SupportAgent from './components/SupportAgent';
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Support Agent Creation Screen</h1>
-      
+      <h1 className="max-w-md mx-auto mt-8 ">Support Agent Creation Screen</h1>
+      <SupportAgent />
+
     </div>
+
   );
 }
 
