6195b02c6dc4bc41d0a06dce40a0b375330dd391
priyans619 <priyansh.2b@gmail.com>
2024-01-19 23:39:30 +0530

    Update App.jsx render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SupportAgent from './components/SupportAgent';
+import SupportTicket from './components/SupportTicket';
 
 
 
@@ -9,6 +10,8 @@ function App() {
       <h1 className="max-w-md mx-auto mt-8 ">Support Agent Creation Screen</h1>
       <SupportAgent />
 
+      <h1 className="max-w-md mx-auto mt-8">Support Ticket Entry Screen</h1>
+      <SupportTicket />
     </div>
 
   );
